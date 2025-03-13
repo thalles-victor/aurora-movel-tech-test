a85da8f1d6e9e94cc512975e921e51fd8e939484
thalles-victor <thalles.victor2012@gmail.com>
2025-03-13 15:29:33 -0300

    :boom: remove: brand.

--- aurora-backend/src/Application/Infra/Repositories/Repositories.module.ts
+++ aurora-backend/src/Application/Infra/Repositories/Repositories.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BrandEntity } from 'src/Application/Entities/Brand.enitty';
 import { CarEntity } from 'src/Application/Entities/Car.entity';
 import { UserEntity } from 'src/Application/Entities/User.entity';
 import { WYSIWYGEntity } from 'src/Application/Entities/WYSIWYG.entity';
 import { UserTypeOrmRepository } from './User/UserTypeOrm.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      UserEntity,
-      BrandEntity,
-      CarEntity,
-      WYSIWYGEntity,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([UserEntity, CarEntity, WYSIWYGEntity])],
   providers: [UserTypeOrmRepository],
   exports: [TypeOrmModule, UserTypeOrmRepository],
 })
--- aurora-backend/src/app.module.ts
+++ aurora-backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ENV } from './@shared/env';
 import { UserEntity } from './Application/Entities/User.entity';
-import { BrandEntity } from './Application/Entities/Brand.enitty';
 import { CarEntity } from './Application/Entities/Car.entity';
 import { WYSIWYGEntity } from './Application/Entities/WYSIWYG.entity';
 import { AuthModule } from './Application/Domains/Auth/Auth.module';
@@ -19,7 +18,7 @@ import { APP_PIPE } from '@nestjs/core';
       database: ENV.POSTGRES_DB,
       username: ENV.POSTGRES_USER,
       password: ENV.POSTGRES_PASSWORD,
-      entities: [UserEntity, BrandEntity, CarEntity, WYSIWYGEntity],
+      entities: [UserEntity, CarEntity, WYSIWYGEntity],
       synchronize: true, // disable when deploying
     }),
     AuthModule,
