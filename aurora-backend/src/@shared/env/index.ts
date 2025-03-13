801acd81a3f01706a6663a440c83ff995cfc7ec3
thalles-victor <thalles.victor2012@gmail.com>
2025-03-13 10:22:30 -0300

    :wrench: conf: postgres with typeorm.

--- aurora-backend/src/@shared/env/index.ts
+++ aurora-backend/src/@shared/env/index.ts
@@ -3,6 +3,13 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
+  POSTGRES_HOST: z.string().nonempty(),
+  POSTGRES_PORT: z
+    .string()
+    .refine((val) => !isNaN(parseInt(val, 10)), {
+      message: 'POSTGRES_PORT must be a valid number',
+    })
+    .transform((val) => parseInt(val, 10)),
   POSTGRES_DB: z.string().nonempty(),
   POSTGRES_USER: z.string().nonempty(),
   POSTGRES_PASSWORD: z.string().nonempty(),
--- aurora-backend/src/app.module.ts
+++ aurora-backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ENV } from './@shared/env';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: ENV.POSTGRES_HOST,
+      port: ENV.POSTGRES_PORT,
+      database: ENV.POSTGRES_DB,
+      username: ENV.POSTGRES_USER,
+      password: ENV.POSTGRES_PASSWORD,
+      entities: [],
+      synchronize: true, // disable when deploying
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
